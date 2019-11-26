dd347540daa70b6fc3933f5078eac2f65327e351
MacStudent <macstudent@126-iMac13.local>
2019-11-25 19:15:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/menuspinerdemo/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.menuspinerdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    private Button btnShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShow = findViewById(R.id.button_show);
+        registerForContextMenu(btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMenu(view);
+            }
+        });
+    }
+
+    public void showMenu(View v) {
+        PopupMenu pop = new PopupMenu(this, v);
+        pop.setOnMenuItemClickListener(this);
+        pop.inflate(R.menu.menu_main);
+        pop.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Log.d("", "");
+                System.out.println("ADD...");
+                break;
+            case R.id.action_detail:
+                System.out.println("Detail...");
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                System.out.println("ADD...");
+                break;
+            case R.id.action_detail:
+                System.out.println("Detail...");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                System.out.println("ADD...");
+                break;
+            case R.id.action_detail:
+                System.out.println("Detail...");
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+}
